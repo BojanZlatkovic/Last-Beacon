17fad2fd4cf0718538a2fe076c3552a54e098b2d
BojanZlatkovic <134451082+BojanZlatkovic@users.noreply.github.com>
2025-11-17 02:55:29 +0100

    GameView.kt

--- app/src/main/java/com/lastbeacon/defense/GameView.kt
+++ app/src/main/java/com/lastbeacon/defense/GameView.kt
@@ -1,1 +1,26 @@
+package com.lastbeacon.defense
 
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var gameView: GameView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        gameView = GameView(this)
+        setContentView(gameView)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        gameView.surfaceDestroyed(gameView.holder)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (!gameView.running) {
+            gameView.surfaceCreated(gameView.holder)
+        }
+    }
+}
